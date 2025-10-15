5ac6eb495af2a1335eec60563b7cc467bd2a1a7b
jacey <jaceygarcia1128@gmail.com>
2025-10-15 13:29:04 -0500

    got the base down for the Home Screen

--- src/main/java/Capstone1.java
+++ src/main/java/Capstone1.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Capstone1 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n Home screen");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment(Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+
+            String choice = scanner.nextLine().toUpperCase();
+
+            switch (choice) {
+                case "D":
+                    addTransaction(true);
+                    break;
+                case "P":
+                    addTransaction(false);
+                    break;
+                case "L":
+                    showLedgerMenu();
+                    break;
+                case "X":
+                    System.out.println("Exiting now, Good bye!");
+                    return;
+
+            }
+        }
+
+
     }
 }
